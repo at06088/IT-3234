3d8c1799b5620f823ebc2633a614c5a5e7121a44
Aston <at06088@georgiasouthern.edu>
2019-02-19 09:57:06 -0500

    Update upload.php
    This code WORKS! but only for april.log
    
    Need to figure out how to make it analyze any file thats inputed

--- upload.php
+++ upload.php
@@ -1,19 +1,18 @@
 <?php
-
 $target_dir = "/var/www/uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
 $fileman = $_FILES["fileToUpload"]["name"];
+
 echo $fileman;
 
-$handle = fopen($target_file,'r') or die ('File opening failed');
+$handle = fopen('april.log','r') or die ('File opening failed');
+
 $requestsCount = 0;
+
 $num404 = 0;
 $lines = 0;
-
 //Finds the 404 and number of lines
 while (!feof($handle)) {
     $lines++;
@@ -23,24 +22,21 @@ while (!feof($handle)) {
     $statusCode = substr($parts[2], 0, 4);
     if (hasRequestType($statusCode, '404')) $num404++;
 }
-
 echo "Total 404 Requests: " . $num404 . "\n";
 echo "Total lines:" . $lines . "\n";
 fclose($handle);
-
 function hasRequestType($l,$s) {
         return substr_count($l,$s) > 0;
 }
-
 //File Download
 /*
 header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename='.basename('file.txt'));
+    header('Content-Disposition: attachment; filename='.basename($fileman));
     header('Expires: 0');
     header('Cache-Control: must-revalidate');
     header('Pragma: public');
-    header('Content-Length: ' . filesize('file.txt'));
-    readfile('file.txt');
+    header('Content-Length: ' . filesize($fileman));
+    readfile($fileman);
     exit;
 */
 ?>
